2c616c224edd96df351c1c860f8bc3554e0c3c6c
anshbegmirza <anshbegmirza@gmail.com>
2025-03-12 10:59:49 +0530

    Test completed.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormArray,
+  FormControl,
+  FormGroup,
+  Validators,
+  AbstractControl,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'angular-6';
+export class AppComponent implements OnInit {
+  genders: string[] = ['Male', 'Female'];
+  countries: string[] = ['India', 'USA', 'England'];
+  signupForm!: FormGroup;
+
+  ngOnInit(): void {
+    this.signupForm = new FormGroup({
+      userData: new FormGroup({
+        firstName: new FormControl(null, [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(50),
+        ]),
+        lastName: new FormControl(null, [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(50),
+        ]),
+      }),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      gender: new FormControl(null, Validators.required),
+      married: new FormControl(false, Validators.required),
+      country: new FormControl(null, Validators.required),
+    });
+  }
+
+  onSubmit() {
+    console.log(this.signupForm.value);
+  }
+
+  onReset() {
+    this.signupForm.reset();
+  }
 }
